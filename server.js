a0994d6c8516cf9d50a6b39f70db23250b52f186
Ashley <lauraashleywalker4@gmail.com>
2022-06-04 14:13:23 -0400

    create server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const path= require('path');
+const express = require('express');
+const routes = require('./controllers/');
+const squelize = require('./config/connections');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false})):
+app.use(express.static(path.join(_dirname, "public")));
+
+//turn onroutes
+app.use(require('./controllers/'));
+
+//turn on connection to db and server
+sequelize.sync({ force:false }).then(() => {
+    app.listen(PORT, () => console.log('NOW LISTENING'));
+ });
+
+
