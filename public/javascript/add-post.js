c35bf634daaa770a3b93dde19edaeb1f2e5372dc
Ashley <lauraashleywalker4@gmail.com>
2022-06-05 09:40:48 -0400

    update javascrip

---
+++ public/javascript/add-post.js
@@ -0,0 +1,27 @@
+const { application } = require("express");
+
+async function newFormHandler(event){
+    event. preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_url = document.querySelector('input[name="post-url"]').value;
+
+    const response= await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_url
+        }),
+        header: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if(response.ok){
+        document.location.replace('/dashboard');
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+
+}
