8bfd7e76be377a403db4fd50116f8883672d63e8
Ashley <lauraashleywalker4@gmail.com>
2022-06-23 22:52:45 -0400

    update controllers

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -38,40 +38,42 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     Post.findOne({
-        where: {
-            id: req.params.id
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         },
-
-        attributes: [
-            'id',
-            'post_text',
-            'title',
-            'created_at',
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-        ]
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     })
-        .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({ message: "No post found with this ID" });
-                return;
-            }
-            res.json(dbPostData);
-        })
-
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 router.post('/', withAuth, (req, res) => {
     console.log(req.body)
@@ -86,7 +88,7 @@ router.post('/', withAuth, (req, res) => {
             res.status(500).json(err);
         });
 });
-
+// update post
 router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
